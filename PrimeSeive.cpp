629af3f52455c141201b6de94a43dcaf88f07ff2
Anmol <techinganmol@gmail.com>
2021-02-04 11:23:58 +0530

    Medium Category

---
+++ PrimeSeive.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+void PrimeSeive(int n)
+{
+    bool prime[n + 1];
+    memset(prime, true, sizeof(prime));
+    for (int i = 2; i * i <= n; i++)
+    {
+        if (prime[i] == true)
+            for (int j = i * i; j <= n; j += i)
+                prime[j] = false;
+    }
+    for (int i = 2; i <= n; i++)
+    {
+        if (prime[i])
+            cout << i << " ";
+    }
+}
+int main()
+{
+    int N;
+    cin >> N;
+    PrimeSeive(N);
+}
